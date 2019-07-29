57c4eec311b55efcdd12a31d879429d2ae048407
Bigshmow <dstewart88@gmail.com>
2019-07-29 15:57:36 -0700

    added gifSearch function and click listener for search which also adds new buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,32 @@
 // alert("make the Gifs!");
-var term = "linda";
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=gx8UOgMleEwf8m4dZ2tJXwNJhSfUR8aD&q=bob+burger+" + term + "&limit=8&offset=0&rating=PG&lang=en";
+function gifSearch(term){
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=gx8UOgMleEwf8m4dZ2tJXwNJhSfUR8aD&q=bob+burger+" + term + "&limit=8&offset=0&rating=PG&lang=en";
     
-
-
     $.ajax({
-      url: queryURL,
+        url: queryURL,
       method: "GET"
     }).then(function(response) {
     
         console.log(response);
-    
+        $("#gifDiv").empty();
+
     for (var i = 0 ; i < 8 ; i++){
    
         var gifStill = $("<img>").attr("src", response.data[i].images.fixed_height.url);
-        
-        $("#gifDiv").append(gifStill);
+        $("#gifDiv").append("<p>" + "Rating: " + response.data[i].rating + "<p>" , gifStill);
     }
+    
+});
+};
 
-    });
+    $("#search-button").on("click", function(event) {
+        event.preventDefault();
+    
+        var inputTerm = $("#search-string").val().trim();
+    
+        gifSearch(inputTerm);
+        var newButton = $("<button>").text(inputTerm);
+        $("#new-buttons").append(newButton);
+      });
