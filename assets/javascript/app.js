a3c697237b64cb2b74c96a656877efccff517461
Bigshmow <dstewart88@gmail.com>
2019-07-31 15:06:28 -0700

    replaced empty buttons with those that contain data-term and added logic for this process when creating a new button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,46 @@
 // alert("make the Gifs!");
 
-function gifSearch(term){
-
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=gx8UOgMleEwf8m4dZ2tJXwNJhSfUR8aD&q=bob+burger+" + term + "&limit=8&offset=0&rating=PG&lang=en";
+$(document).ready(function(){
     
-    $.ajax({
-        url: queryURL,
-      method: "GET"
-    }).then(function(response) {
+    function gifSearch(term){
     
-        console.log(response);
-        $("#gifDiv").empty();
-
-    for (var i = 0 ; i < 8 ; i++){
-   
-        var gifStill = $("<img>").attr("src", response.data[i].images.fixed_height.url);
-        $("#gifDiv").append("<p>" + "Rating: " + response.data[i].rating + "<p>" , gifStill);
-    }
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=gx8UOgMleEwf8m4dZ2tJXwNJhSfUR8aD&q=bob+burger+" + term + "&limit=8&offset=0&rating=PG&lang=en";
+        
+        $.ajax({
+            url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+        
+            console.log(response);
+            $("#gifDiv").empty();
     
-});
-};
-
-    $("#search-button").on("click", function(event) {
+        for (var i = 0 ; i < 8 ; i++){
+       
+            var gifStill = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
+            $("#gifDiv").append("<p>" + "Rating: " + response.data[i].rating + "<p>" , gifStill).attr("class" , "gifBG");
+        }
+        
+        });
+        };
+        $("#search-button").on("click", function(event) {
         event.preventDefault();
+        var inputTerm = $("#search-string").val().trim();   
+            gifSearch(inputTerm);
+            var button = $("<button>");
+            var newButton = button.text(inputTerm).addClass("new-button").attr("data-term" , inputTerm);
+            $("#new-buttons").append(newButton);
+            console.log(inputTerm);
+        $("#search-string").val(" ");         
     
-        var inputTerm = $("#search-string").val().trim();
+        });
+        $(document).on("click", ".new-button" , function(){
+            $("#gifDiv").empty();
+            clickTerm = $(this).attr("data-term")
+            gifSearch(clickTerm);
+            
+        });
     
-        gifSearch(inputTerm);
-        var newButton = $("<button>").text(inputTerm);
-        $("#new-buttons").append(newButton);
-      });
+        
+
+  
+});
