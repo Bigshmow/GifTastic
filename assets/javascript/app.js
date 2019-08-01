c7f799b524e27b67f71db741c91dadad0a82e307
Bigshmow <dstewart88@gmail.com>
2019-07-31 18:28:20 -0700

    finished adding the onclick for playing and pausing gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,8 +16,8 @@ $(document).ready(function(){
     
         for (var i = 0 ; i < 8 ; i++){
        
-            var gifStill = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
-            $("#gifDiv").append("<p>" + "Rating: " + response.data[i].rating + "<p>" , gifStill).attr("class" , "gifBG");
+            var gifStill = $("<img>").attr("src", response.data[i].images.fixed_height_still.url).attr("data-still" , response.data[i].images.fixed_height_still.url).attr("data-animate" , response.data[i].images.fixed_height.url).attr("data-state" , "still").addClass("gif");
+            $("#gifDiv").append("<p>" + "Rating: " + response.data[i].rating + "<p>" , gifStill);
         }
         
         });
@@ -39,6 +39,16 @@ $(document).ready(function(){
             gifSearch(clickTerm);
             
         });
+        $(document).on("click", ".gif" , function() {
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
     
         
 
